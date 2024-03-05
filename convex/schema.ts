ba2a0f2bf0d252005a2078f6eea42686f70e5d6b
Kien Nguyen <kien.nguyen@moveconnect.com>
2024-03-05 21:58:31 +1100

    working on event

---
+++ convex/schema.ts
@@ -0,0 +1,21 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    event: defineTable({
+        name: v.string(),
+        description: v.string(),
+        status: v.string(),
+        xCoordinate: v.number(),
+        yCoordinate: v.number(),
+        tickets: v.array(v.id("ticket")),
+        users : v.array(v.id("user")),
+        comments: v.array(v.id("comment")),
+        eventUrl: v.string(),
+    }),
+    ticket: defineTable({
+        name: v.string(),
+        user: v.id("user"),
+        fee: v.number(),
+    })
+});
