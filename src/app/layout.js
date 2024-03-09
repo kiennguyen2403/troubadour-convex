cf87f00f7fc3a5b5549f8beb9f0b97bd880ae486
Jadenzzz <xauzai2002@gmail.com>
2024-03-10 00:06:49 +1100

    transfered tsx to js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,12 @@
 "use client";
-import type { Metadata } from "next";
+import React from "react";
 import { Inter } from "next/font/google";
-import {
-  ConvexReactClient,
-  Authenticated,
-  Unauthenticated,
-} from "convex/react";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
 import ConvexClientProvider from "./ConvexClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
